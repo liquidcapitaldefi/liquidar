6b0990a622e0366c0c71206d57246e8fcb333a57
grapefi <96745303+grapefi@users.noreply.github.com>
2022-04-11 00:15:36 +1000

    link fix, ignore file

--- src/partials/Cta.jsx
+++ src/partials/Cta.jsx
@@ -13,7 +13,7 @@ function Cta() {
 
             {/* CTA button */}
             <div className="flex justify-center mb-8" data-aos="fade-up" data-aos-delay="400" data-aos-anchor="[data-aos-id-cta]">
-              <a className="btn text-white bg-purple-600 hover:bg-purple-700" href="https://liquidcapital.finance/whitepaper.pdf">Read the Whitepaper</a>
+              <a className="btn text-white bg-purple-600 hover:bg-purple-700" target="_blank" href="https://liquidcapital.finance/whitepaper.pdf">Read the Whitepaper</a>
             </div>
 
             {/* Check list */}
