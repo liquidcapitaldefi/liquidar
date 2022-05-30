b3cd8fb2a9eefa64f2868443fe952b67159c2921
grapefi <96745303+grapefi@users.noreply.github.com>
2022-05-30 13:43:17 +1000

    apr

--- src/partials/Cta.jsx
+++ src/partials/Cta.jsx
@@ -22,7 +22,7 @@ function Cta() {
                 <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                   <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                 </svg>
-                <span>Sustainable Fixed APY</span>
+                <span>Sustainable Variable APY</span>
               </li>
               <li className="flex items-center mx-3 my-1" data-aos="fade-up" data-aos-delay="800" data-aos-anchor="[data-aos-id-cta]">
                 <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
